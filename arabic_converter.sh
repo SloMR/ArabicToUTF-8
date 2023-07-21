bc0ba4a7bf090e0b0c5522f7b8005eb880af17bb
Sulaiman <xSloMxSA@gmail.com>
2023-07-22 01:26:30 +0300

    Add argument and check_input functions.
    - The arguments we have the folder path that he want to check for subtitles.
    - check_input will check for the user input if it's valid or not.

--- arabic_converter.sh
+++ arabic_converter.sh
@@ -7,3 +7,23 @@ function usage() {
     echo "e.g. $0 /Movies/MyMovie"
     exit 1
 }
+
+# Check that the user has provided the correct number of arguments
+if [ $# -lt 1 ]; then
+    usage
+fi
+
+FOLDER_PATH=$1 # e.g. /Movies/MyMovie
+
+# Check that the user has provided the correct arguments
+function check_input() {
+    if [[ -z "$FOLDER_PATH" ]]; then
+        echo "Please provide a folder path as an argument"
+        exit 1
+    fi
+
+    if [[ ! -d "$FOLDER_PATH" ]]; then
+        echo "Folder does not exist"
+        exit 1
+    fi
+}
